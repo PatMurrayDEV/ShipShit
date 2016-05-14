bd550e939e4aabf7fed695012199728a5904e064
Mitchell Busby <mitchbusby@gmail.com>
2016-05-14 19:44:50 +1000

    Adds procfile and webhook

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# Imports the Flask wrapper
-from flask import Flask
+# Imports the Flask wrapper, abort function and request context
+from flask import Flask, abort, request
+
+from werkzeug.contrib.cache import SimpleCache
+
+#Create a little persistent cache object, just for example's sake
+cache = SimpleCache()
+
+from webhook_utils import valid_origin
 
 # Creates an instance of the flask server using *this* module as its unique identifier
 app = Flask(__name__)
@@ -9,6 +16,32 @@ app = Flask(__name__)
 def home():
 	return "Hello world"
 
+@app.route("/webhook", methods=['GET', 'POST'])
+def webhook_receipt():
+
+	if not valid_origin():
+		abort(403)
+
+	if request.method == 'GET':
+		return ''
+
+	if request.method == 'POST':
+		#This is actually a valid webhook, so process it!
+
+		#You can use this handy method inside the request to get the POSTed JSON
+		webhook_data = request.get_json()
+
+		cache.set('webhook_data', webhook_data)
+
+		return 'SUCCESS'
+
+
+@app.route("/past-webhook")
+def past_webhook():
+	#This is a really naive implementation of getting and displaying a past webhook's data
+	return """
+	%s
+	"""  % cache.get('webhook_data')
 
 # Ridiculously simplistic running mechanism
 if __name__ == "__main__":
---
+++ webhook_utils.py
@@ -0,0 +1,3 @@
+def valid_origin():
+	#TODO: validate webhook sender as noted in: https://sendgrid.com/blog/whats-webhook/#securingawebhook
+	return true
